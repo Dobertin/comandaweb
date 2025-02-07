80c750c19d15e4aed4384efb241dabe9e01de891
Roberto Huacca Huamani <robertohuacca@gmail.com>
2025-02-06 21:01:39 -0500

    Primer commit del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LayoutComponent } from './components/layout/layout.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // 👈 Asegura que AppComponent es standalone
+  imports: [LayoutComponent, RouterOutlet], // 👈 Agrega LayoutComponent aquí
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
